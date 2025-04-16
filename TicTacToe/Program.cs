48629a8e770ee2af0b85b9506530d791fb2307ab
Root0zm <phamhoang15032005ii@gmail.com>
2025-04-16 16:28:43 +0700

    Tao Bang

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        private static char [, ] board = new char [3,3];
+        static void Main (string [] args)   {
+            TaoBang();
+            HienThi();
+        }
+        static void TaoBang()
+        {
+            for(int i=0;i<3;i++)
+            {
+                for(int j=0;j<3;j++)
+                   board[i,j]=' ';
+            }
+        }
+        static void HienThi()
+        {
+            Console.WriteLine("  0 1 2");
+            for(int i=0;i<3;i++)
+            {
+                Console.Write($"{i} ");
+                for(int j=0;j<3;j++){
+                    Console.Write(board[i,j]);
+                    if(j<2) Console.Write ("|");
+                }
+                Console.WriteLine();
+                if(i<2) Console.WriteLine("  -+-+-");
+            }
+        }
+    }
+}
--- TicTacToe/obj/Debug/net9.0/TicTacToe.AssemblyInfo.cs
+++ TicTacToe/obj/Debug/net9.0/TicTacToe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e982f639e811773aa78b81187ec0be9ba8e243fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
